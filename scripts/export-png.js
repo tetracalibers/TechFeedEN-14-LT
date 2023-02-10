aaa714d294c514d9ce64493c5a3d13a598f0e80e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-02-10 19:25:03 +0900

    PNGエクスポートスクリプトの修正

--- scripts/export-png.js
+++ scripts/export-png.js
@@ -2,6 +2,10 @@ const puppeteer = require('puppeteer')
 const fs = require('fs/promises')
 const path = require('path')
 
+/**
+ * ディレクトリが存在しなければ作成する関数
+ * @param {string} dirPath 作成したいディレクトリの絶対パス
+ */
 const mkdirIfNotExists = async (dirPath) => {
   try {
     await fs.mkdir(dirPath)
@@ -10,25 +14,44 @@ const mkdirIfNotExists = async (dirPath) => {
   }
 }
 
+/**
+ * for-of文での繰り返し回数カウント用に、1 ~ loopCount + 1 の数列を詰めた配列を作る関数
+ * @param {number} loopCount
+ * @return カウント用の配列（例： loopCountが4なら、 [1, 2, 3, 4, 5]）
+ */
 const generateLoopSeed = (loopCount) => {
   return [...new Array(loopCount)].map((_, i) => i + 1)
 }
 
 ;(async () => {
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  // 解像度を上げる
-  await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 2 })
-
+  /** 生成したPNGを吐き出すディレクトリの絶対パス */
   const DIST_DIR = path.resolve(__dirname, '../export-png')
+  /** devサーバーで立ち上げたスライドのURL */
   const SLIDE_URL = 'http://localhost:3030'
+  /** スライド表示要素のセレクタ。この要素だけをスクショする */
   const CAPTURE_REGION_SELECTOR = '#slide-content'
+  /** 各スライド描画要素に付けられたクラス名。スライド数カウントに使う */
   const PER_PAGE_SELECTOR = '.slidev-layout'
 
+  /** ブラウザのインスタンス */
+  const browser = await puppeteer.launch({ headless: true })
+  /** ブラウザのタブのインスタンス */
+  const page = await browser.newPage()
+  /** Retinaに近い表示に設定 */
+  await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 2 })
+
+  /**
+   * ページの表示が完了するまで待機する関数
+   */
   const waitRender = async () => {
     await page.waitForSelector(CAPTURE_REGION_SELECTOR)
   }
 
+  /**
+   * SPAのページ数をカウントする関数
+   * @param {string} perPageSelector 各ページ描画要素全てに付けられている共通の属性を示すセレクタ
+   * @return カウント用の配列（例： 全5ページなら、[1, 2, 3, 4, 5]）
+   */
   const countPagesSPA = async (perPageSelector) => {
     await waitRender()
 
@@ -40,27 +63,38 @@ const generateLoopSeed = (loopCount) => {
     return generateLoopSeed(pageCount - 1)
   }
 
-  const getClipRegion = async () => {
-    await waitRender(CAPTURE_REGION_SELECTOR)
+  /**
+   * スクショ範囲を指定するための情報を取得する関数
+   * @param {string} captureTargetSelector スクショしたい要素のセレクタ
+   * @return スクショしたい要素のサイズと位置
+   */
+  const getClipRegion = async (captureTargetSelector) => {
+    await waitRender()
 
     const region = await page.evaluate((selector) => {
       const el = document.querySelector(selector)
-      // エレメントの高さと位置を取得
       const { width, height, top: y, left: x } = el.getBoundingClientRect()
       return { width, height, x, y }
-    }, CAPTURE_REGION_SELECTOR)
+    }, captureTargetSelector)
 
     return region
   }
 
+  /**
+   * デバッグログを有効化する関数（※開発中のみ使用を推奨）
+   */
   const debugOn = () => {
     page.on('console', (msg) => console.log('PAGE LOG:', msg.text()))
   }
 
+  /**
+   * 全スライドを順にスクショして、PNGエクスポートを実現する関数
+   */
   const slideExportPng = async () => {
     await page.goto(SLIDE_URL)
 
-    //debugOn()
+    /** 開発時のみ有効化 */
+    // debugOn()
 
     await mkdirIfNotExists(DIST_DIR)
 
@@ -69,13 +103,14 @@ const generateLoopSeed = (loopCount) => {
     for (const i of slides) {
       const clip = await getClipRegion(CAPTURE_REGION_SELECTOR)
       const path = DIST_DIR + '/' + i + '.png'
-      // スクリーンショットに位置と大きさを指定してclipする
       await page.screenshot({ clip, path })
       await page.goto(SLIDE_URL + '/' + (i + 1))
     }
   }
 
+  /** 実行 */
   await slideExportPng()
 
+  /** ブラウザを閉じる */
   browser.close()
 })()
